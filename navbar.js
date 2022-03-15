4b4a61d89f986cc450a51f904fc552b7d2508522
Brooke Rahn <brooke.rahn@gmail.com>
2022-03-14 22:09:08 -0700

    added redirect file

--- navbar.js
+++ navbar.js
@@ -11,7 +11,7 @@ function NavBar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              BadBank Home
+              Bad Bank Home
             </a>
             <button
               className="navbar-toggler"
